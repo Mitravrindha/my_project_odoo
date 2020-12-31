2077842d28b9e68fa3bb4ca9c08adb835119b55c
user <vrindhavinojabd@gmail.com>
2020-12-31 10:25:49 +0530

    Sixteenth Commit

--- reservation_updations/wizards/create_invoice.py
+++ reservation_updations/wizards/create_invoice.py
@@ -10,18 +10,18 @@ class CreateInvoices(models.TransientModel):
         val = []
         invoice_date = fields.datetime.today()
         length = len(self.reserved_ids)
-        for rec in range(length):
+        for rec in self.reserved_ids:
             reserve_lines = []
-            for lines in self.reserved_ids[rec].reservation_lines:
+            for lines in rec.reservation_lines:
                 reserve_lines.append(
                     [0, 0, {'product_id': lines.product_id, 'product_uom_id': lines.product_id.uom_id,
                             'price_unit': lines.product_id.lst_price, 'quantity': lines.product_qty}])
             invoice = self.env['account.move'].create({
                 'move_type': 'out_invoice',
-                'partner_id': self.reserved_ids[rec].customer_id,
+                'partner_id': rec.customer_id,
                 'invoice_date': invoice_date,
                 'invoice_line_ids': reserve_lines,
-                'reference_id': self.reserved_ids[rec]
+                'reference_id': rec
             })
             invoiced_id = int(invoice)
             val.append(invoiced_id)
